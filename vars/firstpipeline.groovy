71667bb4796f118641d6d8ffa96bcc9a5ff5976a
vinaykumarveeraboina <51791892+vinaykumarveeraboina@users.noreply.github.com>
2024-10-28 23:24:32 +0530

    Update firstpipeline.groovy

--- vars/firstpipeline.groovy
+++ vars/firstpipeline.groovy
@@ -1,6 +1,6 @@
 import com.i27Acadamy.Builds.caluculator; 
   //importing build , inside builds we may have many groovys , so we are specifing caluculator(class name ) in import 
-def call(MAP pipelineParams){
+def call(Map pipelineParams){
         library("com.i27.slb")
   caluculator cals = new caluculator(this)  //creating a constructure
 pipeline{
