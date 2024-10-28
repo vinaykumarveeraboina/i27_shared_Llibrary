bbd810df6bd06889dc0a247472b5c646511bbe3d
vinaykumarveeraboina <51791892+vinaykumarveeraboina@users.noreply.github.com>
2024-10-28 23:48:18 +0530

    Update firstpipeline.groovy

--- vars/firstpipeline.groovy
+++ vars/firstpipeline.groovy
@@ -1,4 +1,39 @@
-import com.i27Acadamy.Builds.caluculator; 
+
+
+import com.i27Acadamy.Builds.Calculator
+
+def call(Map pipelineParams){
+    library("com.i27.slb")
+    Calculator calculator = new Calculator(this)
+    pipeline {
+        agent any 
+        environment {
+            APP_NAME = "${pipelineParams.appName}"
+        }
+        stages {
+            stage ('AdditionStage') {
+                steps {
+                    script {
+                        echo "Printing Sum of 2 numbers"
+                        println calculator.add(3,4)
+                        //sub(10,9) // 10-9 = 1
+                        //sub(9,10) //9-10 = -1
+                        echo "Microservice Name is: ${APP_NAME}"
+                    }
+                }
+            }
+            stage ('Nothing') {
+                steps {
+                    echo "Printing text not from groovy files"
+                }
+            }
+        }
+    }
+}
+
+
+
+/*import com.i27Acadamy.Builds.caluculator; 
   //importing build , inside builds we may have many groovys , so we are specifing caluculator(class name ) in import 
 def call(Map pipelineParams){
         library("com.i27.slb")
@@ -20,4 +55,4 @@ pipeline{
     }
    }
 }
-}
+}*/
